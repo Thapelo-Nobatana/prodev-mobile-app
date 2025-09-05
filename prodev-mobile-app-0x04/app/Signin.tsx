0520bb90a84c029aaeae44d3c1466fe3bd8a67f8
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-09-05 15:02:51 +0200

    feat: developed signin.tsx

---
+++ prodev-mobile-app-0x04/app/Signin.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+
+export default function SignIn() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            
+                <Text>Sign In Screen</Text>
+          
+        </View>
+    );
+}
